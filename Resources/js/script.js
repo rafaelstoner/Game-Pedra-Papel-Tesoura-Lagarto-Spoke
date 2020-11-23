e8e4547cc43ad006359386559daccec86ad8b9ef
Rafael Stoner <rafaelstoner7@gmail.com>
2020-11-23 11:27:38 +0200

    Funcionalidades do Computador terminadas

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -2,7 +2,7 @@ const playerScoreEL = document.getElementById('player-score');
 const playerChoiceEl = document.getElementById('player-choice');
 
 const computerScoreEL = document.getElementById('computer-score');
-const computerChoice = document.getElementById('computer-choice');
+const computerChoiceEl = document.getElementById('computer-choice');
 
 const resulText = document.getElementById('result');
 
@@ -43,21 +43,83 @@ const choices = {
     },
 };
 
-
+let computerChoice = '';
 
 // Resetar os icones selecionados
-function resetSelected () {
+function resetSelected() {
     allGameIcons.forEach((icon) => {
         icon.classList.remove('selected')
-        
+
     });
+}
+
+// Escolhas aleatórias do computador
+function computerRandomChoice() {
+    const computerChoiceNumber = Math.random();
+    if (computerChoiceNumber < 0.2) {
+        computerChoice = 'Pedra';
+    } else if (computerChoiceNumber <= 0.4) {
+        computerChoice = 'Papel';
+
+    } else if (computerChoiceNumber <= 0.6) {
+        computerChoice = 'Tesoura';
+
+    } else if (computerChoiceNumber <= 0.8) {
+        computerChoice = 'Lagarto';
+
+    } else {
+        computerChoice = 'Spock';
+    }
+    
+}
+
+// Passando A seleção do computador e dando estilo a seleção
+function displayComputerChoice() {
+    // Dar estilo a icone selecionado e atualizar a seleção
+
+    switch (computerChoice) {
+        case "Pedra":
+            computerRock.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Pedra';
+            break;
+        case "Papel":
+            computerPaper.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Papel';
+            break;
+        case "Tesoura":
+            computerScissor.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Tesoura';
+            break;
+        case "Lagarto":
+            computerLizard.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Lagarto';
+            break;
+        case "Spock":
+            computerSpock.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Spock';
+            break;
+        default:
+            break;
+    }
+
+}
+
+
+
+//Chamar as funções para processar a vez (de jogar)
+function checkResult() {
+    resetSelected();
+    computerRandomChoice();
+    displayComputerChoice();
+    
 }
 
 // Passando A seleção do jogador e dando estilo a seleção
 
 function select(playerChoice) {
     // Dar estilo a icone selecionado e atualizar a seleção
-    resetSelected();
+
+    checkResult();
     switch (playerChoice) {
         case "Pedra":
             playerRock.classList.add('selected');
